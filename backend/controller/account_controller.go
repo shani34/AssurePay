78fbcf976aed5b1e7b08bf135fc2640db2faab31
shani kumar <shanikumar@shanis-MacBook-Air.local>
2023-08-22 22:26:23 +0530

    added implementation of account creation

--- backend/controller/account_controller.go
+++ backend/controller/account_controller.go
@@ -2,10 +2,14 @@ package controller
 
 import (
 	"encoding/json"
+	"fmt"
+	"math/rand"
 	"net/http"
-	"github.com/shani34/AssurePay/backend/models" // Import the Account model
-	"github.com/shani34/AssurePay/backend/connection"
+	"reflect"
+	"time"
 
+	"github.com/shani34/AssurePay/backend/connection"
+	"github.com/shani34/AssurePay/backend/models"
 )
 
 
@@ -16,11 +20,52 @@ func CreateAccountHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
+	
+	if (newAccount.AccountHolder==""|| newAccount.Balance<2500 || 
+	newAccount.Adhaar==""||newAccount.BankName==""||newAccount.FullName==""||newAccount.DOB==""||newAccount.Nominee==""){
+         http.Error(w,"invalid fields",http.StatusBadRequest)
+		 return 
+	}
+
+	w.Header().Set("content-type","application/json")
+
+	var alreadyAccount models.Account
 	db:=connection.DBConnection()
+	err=db.Where("adhaar=?",newAccount.Adhaar).Find(&alreadyAccount).Error
+	if err!=nil{
+	   w.Write([]byte(fmt.Sprint(err)))
+	   w.WriteHeader(http.StatusBadRequest)
+	   return
+	}
+	if !reflect.DeepEqual(alreadyAccount,models.Account{}){
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(alreadyAccount)
+		return 
+	}
+	rand.Seed(time.Now().UnixNano())
+	// Generate a 14-digit random number
+	randomNumber := generateRandomNumber(14)
+	newAccount.AccountNumber=int64(randomNumber)
+	
+	
     db.Create(&newAccount)
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(newAccount)
 	
+}
+func generateRandomNumber(digits int) int {
+	min := intPow(10, digits-1)
+	max := intPow(10, digits) - 1
+
+	return rand.Intn(max-min+1) + min
+}
+
+func intPow(base, exponent int) int {
+	result := 1
+	for i := 0; i < exponent; i++ {
+		result *= base
+	}
+	return result
 }
 
 func GetAccountsHandler(w http.ResponseWriter, r *http.Request) {
--- backend/models/account.go
+++ backend/models/account.go
@@ -8,8 +8,14 @@ import(
 
 type Account struct {
 	ID       uint   `gorm:"primaryKey"`
-	Username string `json:"username"`
+	FullName  string `json:"fullName"`
+	AccountNumber int64 `json:"accountNumber"`
+	AccountHolder string `json:"accountHolder"`
+	DOB			string `json:"dob"`
+	Nominee		string `json:"nominee"`
+	Adhaar  		string 	`json:"adhaar"`
 	Balance  float64 `json:"balance"`
+	BankName	string `json:"bankName"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	DeletedAt gorm.DeletedAt `gorm:"index"`
