5381782042b64924a60000cf24a1e8725dcb2267
shani kumar <shanikumar@shanis-MacBook-Air.local>
2023-08-23 01:28:43 +0530

    integrated with backend

--- frontend/assure-pay/src/components/Accounts.js
+++ frontend/assure-pay/src/components/Accounts.js
@@ -2,72 +2,95 @@ import React, { useState } from 'react';
 import CreateForm from'./account.module.css'
 import styles from './welcome.module.css';
 import {useNavigate} from 'react-router-dom'
+import axios from '../api/axios';
 // import bankLogo from './bank-logo.png'; // Import your logo image
 
 const CreateAccountForm = () => {
-  const [accountNumber, setAccountNumber] = useState('');
+  const [fullName, setFullName] = useState('');
   const [accountHolder, setAccountHolder] = useState('');
   const [dob, setDob] = useState('');
   const [nominee, setNominee] = useState('');
   const [balance, setBalance] = useState('');
   const [bankName, setBankName] = useState('');
   const [adhaar, setAdhaar]=useState('');
+  const [email, setMail]=useState('');
   const navigate =useNavigate();
   const bankNames = ['Bank A', 'Bank B', 'Bank C', 'Bank D'];
   const [errorMessages, setErrorMessages] = useState({
-    accountNumber: '',
+    fullName: '',
     accountHolder: '',
     dob: '',
     nominee: '',
     balance: '',
     bankName: '',
     adhaar: '',
+    email:'',
   });
   const handleSubmit = (e) => {
     const errors={};
     e.preventDefault();
     try{
-    if (accountNumber.length<14){
-        errors.accountNumber="Invalid length of account number";
-    }
-    if (adhaar.length<14){
-      errors.adhaar="Invalid length of adhar number number";
-     
-    }
-    const dobDate = new Date(dob);
-    const yearOfBirth = dobDate.getFullYear();
-    const currentYear=new Date().getFullYear();
-    if(yearOfBirth>currentYear){
-      errors.dob="Invalid length of DOB number";
-    }
+        if (fullName.length>14){
+            errors.accountNumber="Invalid length of account number";
+        }
+        if (adhaar.length<14){
+          errors.adhaar="Invalid length of adhar number number";
+        
+        }
+      const dobDate = new Date(dob);
+      const yearOfBirth = dobDate.getFullYear();
+      const currentYear=new Date().getFullYear();
+      if(yearOfBirth>currentYear){
+        errors.dob="Invalid length of DOB number";
+      }
 
-    if (Object.keys(errors).length > 0) {
-      setErrorMessages(errors);
-      return; // Return early if any required field is missing
-    }
+      if (Object.keys(errors).length > 0) {
+        setErrorMessages(errors);
+        return; // Return early if any required field is missing
+      }
     // Here you can perform actions like sending the form data to a server or updating state
-    console.log('Form submitted:', {
-      accountNumber,
-      accountHolder,
-      dob,
-      nominee,
-      balance,
-      bankName,
-    });
+     const data= JSON.stringify({
+        fullName: fullName,
+        accountHolder:accountHolder,
+        dob: dob,
+        nominee: nominee,
+        adhaar: adhaar,
+        balance: parseFloat(balance),
+        email:email,
+        bankName: bankName
+      })
+    
+      const Response=axios.post("/api/accounts",data,{
+        withCredentials: true
+      })
+     
     // Clear the form fields
-    setAccountNumber('');
+    setFullName('');
     setAccountHolder('');
     setDob('');
     setNominee('');
     setBalance('');
     setBankName('');
     setAdhaar('');
+    setMail('');
 
-    window.alert("form successfully submitted!");
+    window.alert("form successfully submitted",JSON.stringify(Response.accountNumber));
     navigate('/welcome');
   } catch(error){
-        console.error(error);
-        return;
+    if (error.response && error.response.status) {
+      const statusCode=error.response.status
+      if (statusCode === 200) {
+          console.log('User logged in successfully');
+          // Handle successful login
+        } else if (statusCode === 401) {
+          window.alert('Login failed: Invalid credentials');
+          // Handle unauthorized access
+        } else {
+          window.alert('An error occurred');
+          // Handle other error cases
+        }
+      }
+    console.log('An error occurred hitting the end point in',error);
     }
   };
 
@@ -77,12 +100,12 @@ const CreateAccountForm = () => {
       {/* <img src={bankLogo} alt="Bank Logo" className="logo" /> */}
       <h2>Create a New Bank Account</h2>
       <form onSubmit={handleSubmit}>
-        <div>{errorMessages.accountNumber}
+        <div>{errorMessages.fullName}
           <label>Full Name:</label>
           <input
             type="text"
-            value={accountNumber}
-            onChange={(e) => setAccountNumber(e.target.value)}
+            value={fullName}
+            onChange={(e) => setFullName(e.target.value)}
             required
           />
         </div>
@@ -129,6 +152,16 @@ const CreateAccountForm = () => {
             value={balance}
             placeholder='min balance : 2500'
             onChange={(e) => setBalance(e.target.value)}
+            required
+          />
+        </div>
+        <div>{errorMessages.email}
+          <label>Email-Id:</label>
+          <input
+            type="email"
+            value={email}
+            placeholder='email'
+            onChange={(e) => setMail(e.target.value)}
             required
           />
         </div>
